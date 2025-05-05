4984139ab1beada9880088405edc1213a9d4da47
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-05-05 10:52:37 +0800

    GameSpace: fix and animate panel view translation
    Change-Id: Ie8ef617160a21823d73b40f125f4be72c5296dd0
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/gamebar/GameBarService.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/gamebar/GameBarService.kt
@@ -122,7 +122,6 @@ class GameBarService : Hilt_GameBarService() {
                 if (!::rootPanelView.isInitialized) {
                     setupPanelView()
                 }
-                updatePanelTranslation()
                 if (!rootPanelView.isAttachedToWindow) {
                     wm.addView(rootPanelView, panelLayoutParam)
                     rootPanelView.alpha = 0f
@@ -347,14 +346,6 @@ class GameBarService : Hilt_GameBarService() {
         } else {
             rootPanelView.gravity = Gravity.END
             rootPanelView.setPaddingRelative(16, 16, barWidth, 16)
-        }
-    }
-    
-    fun updatePanelTranslation() {
-        panelView.post {
-            val layoutParams = panelView.layoutParams as ViewGroup.MarginLayoutParams
-            layoutParams.topMargin = appSettings.y
-            panelView.layoutParams = layoutParams
         }
     }
 

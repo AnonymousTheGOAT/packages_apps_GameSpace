dc1aaf788ac4ccc5c1e5744444d4d9b98e7610a3
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-05-07 05:11:20 +0900

    GameSpace: Launch game when click header
    Change-Id: Ib6f4078fc78ac6e7ea0eebb39a7dc0696881de47

--- app/src/main/java/io/chaldeaprjkt/gamespace/settings/PerAppSettingsFragment.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/settings/PerAppSettingsFragment.kt
@@ -20,6 +20,7 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Process
 import android.view.View
 import androidx.preference.ListPreference
 import androidx.preference.Preference
@@ -31,6 +32,7 @@ import io.chaldeaprjkt.gamespace.data.GameConfig
 import io.chaldeaprjkt.gamespace.data.SystemSettings
 import io.chaldeaprjkt.gamespace.data.UserGame
 import io.chaldeaprjkt.gamespace.utils.GameModeUtils
+import io.chaldeaprjkt.gamespace.utils.getAppInfoForPackage
 import javax.inject.Inject
 
 @AndroidEntryPoint(PreferenceFragmentCompat::class)
@@ -68,6 +70,24 @@ class PerAppSettingsFragment : Hilt_PerAppSettingsFragment(),
             layoutResource = R.layout.per_app_header
             icon = currentGame?.loadIcon(context.packageManager)
             title = context.packageManager?.let { currentGame?.loadLabel(it) }
+            setOnPreferenceClickListener {
+                currentGame?.packageName?.let {
+                    val appActivity = context.getAppInfoForPackage(it)
+                    if (appActivity != null) {
+                        val intent =
+                            Intent().apply {
+                                setComponent(appActivity.componentName)
+                                addFlags(
+                                    Intent.FLAG_ACTIVITY_NEW_TASK or
+                                        Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED or
+                                            Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT
+                                )
+                            }
+                        context.startActivityAsUser(intent, Process.myUserHandle())
+                    }
+                }
+                true
+            }
         }
         findPreference<ListPreference>(PREF_PREFERRED_MODE)?.apply {
             currentConfig?.mode?.let { value = it.toString() }

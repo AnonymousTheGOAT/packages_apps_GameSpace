23e26fb329b7637b44abd70779b70166515dfe65
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-09-04 21:26:41 +0900

    GameSpace: Move rest of fragments to material expressive design
    Change-Id: I3bc1235377f1c1fbc9267b75b790c4601a6a3cff

--- app/src/main/java/io/chaldeaprjkt/gamespace/settings/GameListSettingsFragment.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/settings/GameListSettingsFragment.kt
@@ -22,13 +22,13 @@ import android.provider.Settings
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.preference.Preference
-import androidx.preference.PreferenceFragmentCompat
+import com.android.settingslib.widget.SettingsBasePreferenceFragment
 import dagger.hilt.android.AndroidEntryPoint
 import io.chaldeaprjkt.gamespace.R
 import io.chaldeaprjkt.gamespace.preferences.AppListPreferences
 import io.chaldeaprjkt.gamespace.preferences.appselector.AppSelectorActivity
 
-@AndroidEntryPoint(PreferenceFragmentCompat::class)
+@AndroidEntryPoint(SettingsBasePreferenceFragment::class)
 class GameListSettingsFragment : Hilt_GameListSettingsFragment() {
 
     private var apps: AppListPreferences? = null

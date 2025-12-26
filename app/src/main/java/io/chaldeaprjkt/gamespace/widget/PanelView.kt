b3cd9c31a9d79ac74513996e78fae62199a09a16
Lup Gabriel <gwolf2u.website@gmail.com>
2025-12-26 19:41:29 +0200

    GameSpace: adjust panel position on screen based on available space
    Fixes https://github.com/crdroidandroid/issue_tracker/issues/779
    
    Change-Id: If40819025cbd5a11117ff160bafe774bd2a1ae9c

--- app/src/main/java/io/chaldeaprjkt/gamespace/widget/PanelView.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/widget/PanelView.kt
@@ -9,10 +9,13 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.view.animation.DecelerateInterpolator
 import android.widget.LinearLayout
+import androidx.core.view.doOnLayout
 import io.chaldeaprjkt.gamespace.R
 import io.chaldeaprjkt.gamespace.utils.di.ServiceViewEntryPoint
+import io.chaldeaprjkt.gamespace.utils.dp
 import io.chaldeaprjkt.gamespace.utils.entryPointOf
 
 class PanelView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) :
@@ -27,16 +30,27 @@ class PanelView @JvmOverloads constructor(context: Context, attrs: AttributeSet?
     }
 
     fun updateTranslationY() {
-        val targetMargin = appSettings.y
-        val params = layoutParams as ViewGroup.MarginLayoutParams
-        val animator = ValueAnimator.ofInt(params.topMargin, targetMargin)
-        animator.duration = 300L
-        animator.interpolator = DecelerateInterpolator()
-        animator.addUpdateListener { valueAnimator ->
-            params.topMargin = valueAnimator.animatedValue as Int
-            layoutParams = params
+        doOnLayout {
+            val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+            val screenHeight = wm.maximumWindowMetrics.bounds.height()
+            val panelHeight = measuredHeight
+
+            val targetMargin = appSettings.y
+            val params = layoutParams as ViewGroup.MarginLayoutParams
+
+            // Ensure the panel doesn't go off-screen
+            val maxMargin = screenHeight - panelHeight - 16.dp
+            val adjustedMargin = targetMargin.coerceIn(0, maxMargin)
+
+            val animator = ValueAnimator.ofInt(params.topMargin, adjustedMargin)
+            animator.duration = 300L
+            animator.interpolator = DecelerateInterpolator()
+            animator.addUpdateListener { valueAnimator ->
+                params.topMargin = valueAnimator.animatedValue as Int
+                layoutParams = params
+            }
+            animator.start()
         }
-        animator.start()
     }
 
     override fun onAttachedToWindow() {
